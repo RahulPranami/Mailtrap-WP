b92abd9ca74e1783600e6c5ca7bf54cc00782f98
Rahul Pranami <rahulpranami101@gmail.com>
2024-02-08 21:42:21 +0530

    Initial Commit :tada:

---
+++ includes/class-mailtrap-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://rahulpranami.co
+ * @since      1.0.0
+ *
+ * @package    Mailtrap
+ * @subpackage Mailtrap/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Mailtrap
+ * @subpackage Mailtrap/includes
+ * @author     Rahul Pranami <rahulpranami101@gmail.com>
+ */
+class Mailtrap_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-mailtrap-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://rahulpranami.co
+ * @since      1.0.0
+ *
+ * @package    Mailtrap
+ * @subpackage Mailtrap/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Mailtrap
+ * @subpackage Mailtrap/includes
+ * @author     Rahul Pranami <rahulpranami101@gmail.com>
+ */
+class Mailtrap_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
